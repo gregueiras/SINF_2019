c88f3ae16c463dcbf38cc2f5bb3d2f32433ad699
susanalima <up201603634@fe.up.pt>
2019-11-20 18:23:27 +0000

    add overview table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Menu from './components/Menu/Menu';
+import Overview from './components/Overview/Overview';
+
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
          <div className="App">
           <NavBar />
           <Menu />
+          <Overview/>
         </div>
       </Router>
   
