77054d76128a75294785b2fb76c06e94d3eeb29c
Joana SIlva <up201208979@fe.up.pt>
2019-11-25 19:02:36 +0000

    Fix responsiveness of navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -10,17 +10,21 @@ function NavBar() {
     <Container>
       <Row>
         <Navbar expand="lg" variant="light" fixed="top">
-          <Col sm={8}>
-            <Link to="/" className="logoTextLink">
-              <div className="logoText">
-                        s i n t e r
-              </div>
-            </Link>
+          <Col xs={12} md={8} xs={8} className="logo">
+            <Navbar.Brand>
+              <Link to="/" className="logoTextLink">
+                <div className="logoText">
+                          s i n t e r
+                </div>
+              </Link>
+            </Navbar.Brand>
           </Col>
-          <Col sm={4} id="navbarLogout">
-            <Link to="#logout" className="logoutLink">
-                    Logout
-            </Link>
+          <Col xs={6} md={4} xs={4}id="navbarLogout">
+            <Navbar.Brand>
+              <Link to="#logout" className="logoutLink">
+                      Logout
+              </Link>
+             </Navbar.Brand>
           </Col>
         </Navbar>
       </Row>
