147383cbe5ad2022c2ba19b4870e82631a0ac8eb
Rita <ritanorinho98@hotmail.com>
2019-12-13 10:49:02 +0000

    progress on sg_rg

--- backend/jobs/SG_RG.js
+++ backend/jobs/SG_RG.js
@@ -4,6 +4,8 @@ import {
   getProcessOrder,
   getCompanyName,
   getCompanyKey,
+  getSalesOrders,
+  getPurchasesOrders,
 } from "../services/jasmin";
 import {
   isProcessed,
@@ -11,6 +13,7 @@ import {
   getCorrespondence,
   getCustomerParty,
   getSellerParty,
+  getCorrespondenceB,
 } from "../services/db";
 import Queue from "../lib/Queue";
 import getShippingDeliveries from "../services/jasmin/getShippingDeliveries";
@@ -40,13 +43,39 @@ export default {
       companyB
     });
     const companyBKey = await getCompanyKey({ companyID: companyB });
-    console.log("company b " + companyBKey);
+    const companyAKey = await getCompanyKey({ companyID: companyA });
+    console.log("company a " + companyAKey);
     let shippingDeliveriesData, processOrdersData;
+
+    const userID = 1;
+
+    const info = {
+      userID,
+      processID: 1,
+      companyA,
+      companyB
+    };
     try {
       shippingDeliveriesData = (await getShippingDeliveries({
         companyID: companyB
       }
       )).data;
+      let purchasesOrdersData, salesOrdersData;
+      try {
+        purchasesOrdersData = (await getPurchasesOrders({ companyID: companyA, processID }))
+          .data;
+        salesOrdersData = (await getSalesOrders({ companyID: companyB, processID })).data;
+      } catch (e) {
+        console.error(e.response.data);
+      }
+
+      const purchaseOrders = purchasesOrdersData.filter(
+        po =>
+          po.isActive &&
+          !po.isDeleted &&
+          po.sellerSupplierParty == sellerParty
+      );
+
 
       const shippingDeliveries = shippingDeliveriesData.filter(pi => pi.isActive && !pi.isDeleted && pi.party == customerParty && pi.documentStatus === 1);
 
@@ -58,21 +87,92 @@ export default {
           options
         });
       }
-      console.log(shippingDeliveries);
-      for(const shippingDelivery of shippingDeliveries){
-        
+      let areNewDocuments = false;
+
+      for (const shippingDelivery of shippingDeliveries) {
+        const replicated = await isProcessed({ userID, fileID: shippingDelivery.id });
+        if (!replicated) {
+          areNewDocuments = true;
+          console.log("new documents");
+          const documentLines = [];
+          let abort = false;
+          for (const line of shippingDelivery.documentLines) {
+            const salesOrder = salesOrdersData.filter(
+              so =>
+                so.naturalKey == line.sourceDoc
+            )[0];
+            console.log("sales order filter");
+            let foundMatchingSI;
+            for (const si of purchaseOrders) {
+              console.log("sales order  "+JSON.stringify(si.grossValue.amount)+" "+JSON.stringify(salesOrder.grossValue.amount));
+
+              if (si.payableAmount.amount === salesOrder.grossValue.amount){
+                let equal = true;
+                console.log("purchase filter");
+
+                for (const line1 of si.documentLines) {
+                  console.log("line1 filter");
+                  const found = salesOrder.documentLines.some(
+                    async el =>
+                      el.grossValue.reportingAmount ===
+                      line1.grossValue.reportingAmount &&
+                      el.grossValue.amount === line1.grossValue.amount &&
+                      el.grossValue.baseAmount === line1.grossValue.baseAmount &&
+                      el.taxExclusiveAmount.reportingAmount ===
+                      line1.taxExclusiveAmount.reportingAmount &&
+                      el.taxExclusiveAmount.amount ===
+                      line1.taxExclusiveAmount.amount &&
+                      el.taxExclusiveAmount.baseAmount ===
+                      line1.taxExclusiveAmount.baseAmount &&
+                      el.unitPrice.reportingAmount ===
+                      line1.unitPrice.reportingAmount &&
+                      el.unitPrice.amount === line1.unitPrice.amount &&
+                      el.unitPrice.baseAmount === line1.unitPrice.baseAmount &&
+                      el.quantity === line1.quantity &&
+                      el.lineExtensionAmount.reportingAmount ===
+                      line1.lineExtensionAmount.reportingAmount &&
+                      el.lineExtensionAmount.amount ===
+                      line1.lineExtensionAmount.amount &&
+                      el.lineExtensionAmount.baseAmount ===
+                      line1.lineExtensionAmount.baseAmount &&
+                      el.salesItem === (await getCorrespondenceB({ companyA, companyB, product: line1.purchasesItem }))
+                  );
+
+                  equal &= found;
+                }
+                if (equal) {
+                  foundMatchingSI = si;
+                  console.log("FOUND   "+ foundMatchingSI);
+                  break;
+                }
+              }
+
+            }
+          }
+          /*const {
+            sourceDocKey: line.sourceDoc,
+            sourceDocLineNumber: 
+          } = data;*/
+
+          let sourceDocKey = "ECF.2019.3";
+          let sourceDocLineNumber = 1;
+          let quantity = 1;
+        }
+        if (!abort) {
+
+          console.log("abort " + abort);
+          console.log("company a " + companyAKey);
+          /* Queue.add("create_RG", {sourceDocKey: "ECF.2019.3", 
+           sourceDocLineNumber: 1, quantity: 1,
+            shippingDelivery: shippingDelivery,
+            processID, userID, companyKey: companyAKey, companyID: companyA
+           });*/
+        }
+
       }
-    } catch (e) {
-      console.log(e.response.data);
-    }
-
-    const userID = 1;
-
-    const info = {
-      userID,
-      processID: 1,
-      companyA,
-      companyB
-    };
+    } catch(e) {
+    console.log(e.response.data);
   }
+
+}
 };
