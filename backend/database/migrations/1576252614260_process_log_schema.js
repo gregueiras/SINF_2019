0f6fe41baf5b2280f0c438475d3bcbfe93e71789
susanalima <up201603634@fe.up.pt>
2019-12-14 15:31:55 +0000

    add missing file

--- backend/database/migrations/1576252614260_process_log_schema.js
+++ backend/database/migrations/1576252614260_process_log_schema.js
@@ -7,6 +7,14 @@ class ProcessLogSchema extends Schema {
   up () {
     this.create('process_logs', (table) => {
       table.increments();
+      table.bigInteger('user').notNullable();
+      table.foreign('user').references('id').inTable('users');
+      table.bigInteger('company_a').notNullable();
+      table.bigInteger('company_b').notNullable();
+      table.foreign('company_a').references('id').inTable('companies').onDelete('CASCADE');
+      table.foreign('company_b').references('id').inTable('companies').onDelete('CASCADE');
+      table.bigInteger('process_type').notNullable();
+      table.foreign('process_type').references('id').inTable('process_types').onDelete('CASCADE');
       table.timestamps();
     })
   }
