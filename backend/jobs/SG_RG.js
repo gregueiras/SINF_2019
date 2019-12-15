c220c31557bb1367cf148b91beadf9ac3b3fa241
susanalima <up201603634@fe.up.pt>
2019-12-15 14:05:50 +0000

    small changes to SG_RG

--- backend/jobs/SG_RG.js
+++ backend/jobs/SG_RG.js
@@ -6,6 +6,7 @@ import {
   getCompanyKey,
   getSalesOrders,
   getPurchasesOrders,
+  getSeries as getJasminSeries
 } from "../services/jasmin";
 import {
   isProcessed,
@@ -13,8 +14,12 @@ import {
   getCorrespondence,
   getCustomerParty,
   getSellerParty,
+  getSeries as getProcessSeries,
   getCorrespondenceB,
-  getPurchaseOrderCorrespondence
+  getPurchaseOrderCorrespondence,
+  isMyTurn,
+  nextTurn,
+  setFailedStep
 } from "../services/db";
 import Queue from "../lib/Queue";
 import getShippingDeliveries from "../services/jasmin/getShippingDeliveries";
@@ -33,21 +38,6 @@ export default {
   async handle({ data }, done) {
     const { companyA, companyB, processID, step } = data;
 
-    const customerParty = await getCustomerParty({
-      companyA,
-      companyB
-    });
-    console.log("customer party " + customerParty);
-
-    const sellerParty = await getSellerParty({
-      companyA,
-      companyB
-    });
-    const companyBKey = await getCompanyKey({ companyID: companyB });
-    const companyAKey = await getCompanyKey({ companyID: companyA });
-    console.log("company a " + companyAKey);
-    let shippingDeliveriesData, processOrdersData;
-
     const userID = 1;
 
     const info = {
@@ -56,84 +46,184 @@ export default {
       companyA,
       companyB
     };
-    try {
-      shippingDeliveriesData = (await getShippingDeliveries({
-        companyID: companyB
-      }
-      )).data;
-      let purchasesOrdersData, salesOrdersData;
+
+    const active = await isMyTurn({ processID, step });
+
+    if (!active) {
+      done(null, {
+        value: RETURN_TYPES.END_INVALID_STEP,
+        msg: `Invalid step ${step}`,
+        ...info,
+        options
+      });
+    } else {
+      const customerParty = await getCustomerParty({
+        companyA,
+        companyB
+      });
+      console.log("customer party " + customerParty);
+
+      const sellerParty = await getSellerParty({
+        companyA,
+        companyB
+      });
+
+      let series;
       try {
-        purchasesOrdersData = (await getPurchasesOrders({ companyID: companyA, processID }))
+        series = (await getJasminSeries({ companyID: companyA, processID }))
           .data;
-        salesOrdersData = (await getSalesOrders({ companyID: companyB, processID })).data;
-      } catch (e) {
-        console.error(e.response.data);
+      } catch (error) {
+        console.log("ERROR SERIES");
+        console.error(error.response.data);
       }
 
-      const purchaseOrders = purchasesOrdersData.filter(
-        po =>
-          po.isActive &&
-          !po.isDeleted &&
-          po.sellerSupplierParty == sellerParty
-      );
+      const serieKey = await getProcessSeries({ processID });
+      const serie = series.find(({ serieKey: sK }) => sK === serieKey);
+      console.log("SERIE " + serie + " " + serieKey);
 
-
-      const shippingDeliveries = shippingDeliveriesData.filter(pi => pi.isActive && !pi.isDeleted && pi.party == customerParty && pi.documentStatus === 1);
-
-      if (!shippingDeliveries) {
-        done(null, {
-          value: RETURN_TYPES.END_TRIGGER_FAIL,
-          msg: `No process orders found. Please check if you have defined it correctly.`,
-          ...info,
-          options
-        });
+      if (serie === undefined) {
+        console.log(`ERROR: NO SERIES ${serieKey}`);
+        //console.error(e.response.data);
+        await setFailedStep({ processID });
+        done(null, { msg: `ERROR: NO SERIES ${serieKey}` });
+        return;
       }
-      let areNewDocuments = false;
-      let sourceDocKey, quantity, sourceDocLineNumber;
 
-      for (const shippingDelivery of shippingDeliveries) {
-        const replicated = await isProcessed({ userID, fileID: shippingDelivery.id });
-        if (!replicated) {
-          areNewDocuments = true;
-          console.log("new documents");
-          const documentLines = [];
-          let abort = false;
-          for (const line of shippingDelivery.documentLines) {
-            const salesOrder = salesOrdersData.filter(
-              so =>
-                so.naturalKey == line.sourceDoc
-            )[0];
-            const purchaseOrderId = await getPurchaseOrderCorrespondence({salesOrder: salesOrder.id});
-            if (purchaseOrderId !== undefined){
-            for(const purchaseOrder of purchaseOrders){
-            
-              
-              if (purchaseOrder.id === purchaseOrderId){
-                sourceDocKey = purchaseOrder.naturalKey;
-                //documentLines.push({sourceDocKey, sourceDocLine, quantity});
-                console.log("PURCHASE ORDER ");
-              } 
+
+      const companyBKey = await getCompanyKey({ companyID: companyB });
+      const companyAKey = await getCompanyKey({ companyID: companyA });
+
+      console.log("company a " + companyAKey);
+
+      let shippingDeliveriesData, processOrdersData;
+
+      try {
+        shippingDeliveriesData = (
+          await getShippingDeliveries({
+            companyID: companyB
+          })
+        ).data;
+        let purchasesOrdersData, salesOrdersData;
+        try {
+          purchasesOrdersData = (
+            await getPurchasesOrders({ companyID: companyA, processID })
+          ).data;
+          salesOrdersData = (
+            await getSalesOrders({ companyID: companyB, processID })
+          ).data;
+        } catch (e) {
+          console.error(e.response.data);
+        }
+
+        const purchaseOrders = purchasesOrdersData.filter(
+          po =>
+            po.serie === serieKey &&
+            po.isActive &&
+            !po.isDeleted &&
+            po.sellerSupplierParty == sellerParty
+        );
+
+        const shippingDeliveries = shippingDeliveriesData.filter(
+          pi =>
+            pi.serie === serieKey &&
+            pi.isActive &&
+            !pi.isDeleted &&
+            pi.party == customerParty &&
+            pi.documentStatus === 1
+        );
+
+        if (!shippingDeliveries) {
+          done(null, {
+            value: RETURN_TYPES.END_TRIGGER_FAIL,
+            msg: `No process orders found. Please check if you have defined it correctly.`,
+            ...info,
+            options
+          });
+        }
+        let areNewDocuments = false;
+        let sourceDocKey, quantity, sourceDocLineNumber;
+
+        for (const shippingDelivery of shippingDeliveries) {
+          const replicated = await isProcessed({
+            userID,
+            fileID: shippingDelivery.id
+          });
+          if (!replicated) {
+            areNewDocuments = true;
+            console.log("new documents");
+            const documentLines = [];
+            let abort = false;
+            for (const line of shippingDelivery.documentLines) {
+              const salesOrder = salesOrdersData.filter(
+                so => so.naturalKey == line.sourceDoc
+              )[0];
+              const purchaseOrderId = await getPurchaseOrderCorrespondence({
+                salesOrder: salesOrder.id
+              });
+              if (purchaseOrderId !== undefined) {
+                for (const purchaseOrder of purchaseOrders) {
+                  if (purchaseOrder.id === purchaseOrderId) {
+                    sourceDocKey = purchaseOrder.naturalKey;
+                    //documentLines.push({sourceDocKey, sourceDocLine, quantity});
+                    console.log("PURCHASE ORDER ");
+                  }
+                }
+              } else abort = true;
+              quantity = line.quantity;
+              sourceDocLineNumber = line.sourceDocLine;
+              console.log("line " + quantity + " " + sourceDocLineNumber);
             }
-          }else abort = true;
-          quantity = line.quantity;
-          sourceDocLineNumber = line.sourceDocLine;
-          console.log("line "+quantity+" "+sourceDocLineNumber);
+            if (!abort) {
+              Queue.add("create_RG", {
+                sourceDocKey,
+                sourceDocLineNumber,
+                quantity,
+                shippingDelivery: shippingDelivery,
+                processID,
+                userID,
+                companyKey: companyAKey,
+                companyID: companyA
+              });
+            }
+          }
         }
-        if (!abort) {
-          console.log("abort " + abort);
-          console.log("company a " + companyAKey);
-           Queue.add("create_RG", {sourceDocKey, 
-           sourceDocLineNumber , quantity,
-            shippingDelivery: shippingDelivery,
-            processID, userID, companyKey: companyAKey, companyID: companyA
-           });
+        if (!areNewDocuments) {
+          console.log("NO NEW RES SG_RG");
+          await setFailedStep({ processID });
+          done(null, {
+            result: RETURN_TYPES.END_NO_NEW_DOCUMENTS,
+            ...info,
+            options
+          });
+        } else {
+          await nextTurn({ processID });
+          done(null, {
+            value: RETURN_TYPES.END_SUCCESS,
+            ...info,
+            options
+          });
         }
-
+      } catch (e) {
+        if (e.response) {
+          console.error(e.response.data);
+          await setFailedStep({ processID });
+          done(null, {
+            value: RETURN_TYPES.END_ACTION_FAIL,
+            data: e.response.data,
+            ...info,
+            options
+          });
+        } else {
+          console.error(e);
+          await setFailedStep({ processID });
+          done(null, {
+            value: RETURN_TYPES.END_ACTION_FAIL,
+            ...info,
+            data: e,
+            options
+          });
+        }
       }
     }
-    } catch(e) {
-    console.log(e.response.data);
   }
-
-}
 };
